474af0002c9e7bf7a311432ca49bed5d1830652e
Md Aatif N Tegginmani <mdaatif84@gmail.com>
2022-12-13 01:43:17 +0530

    obj

--- index.js
+++ index.js
@@ -7,15 +7,25 @@ function savetoLocalstorage(event){
     const phone = document.getElementById('phone').value
     const date = document.getElementById('date').value
     const time = document.getElementById('time').value
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
-    localStorage.setItem('phone', phone);
-    localStorage.setItem('Date', date);
-    localStorage.setItem('time', time);
+
+
+    // localStorage.setItem('name', name);
+    // localStorage.setItem('email', email);
+    // localStorage.setItem('phone', phone);
+    // localStorage.setItem('Date', date);
+    // localStorage.setItem('time', time);
+
+
+    let myObj= {
+        name : name ,
+        email :email ,
+        phone : phone ,
+        date : date ,
+        time : time 
+     }
+     localStorage.setItem("newObj" , JSON.stringify(myObj))
+
     }
-
-
-
 
 
 
